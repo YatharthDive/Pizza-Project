7b2ba5367b23b7f412cfc012e782e94b298b0aac
YatharthDive <71880750+YatharthDive@users.noreply.github.com>
2021-05-20 14:56:08 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ menu_list_array = ["Chicken Tandoori Pizza",
                   "Veg Extravaganza Pizza"];
 function getmenu(){
     var htmldata;
-    htmldata="<ol class='menulist'>"
+    htmldata="<ol class='menu_div'>"
     menu_list_array.sort();
     for(var i=0;i<menu_list_array.length;i++){
         htmldata=htmldata+'<li>' + menu_list_array[i] + '</li>'
@@ -18,11 +18,11 @@ function add_item(){
     var item=document.getElementById("add_item").value;
     menu_list_array.push(item);
     menu_list_array.sort();
-    htmldata="<section class='cards'>"
-    for(var i=0;menu_list_array.length;i++){
+    htmldata="<div class='cards'>"
+    for(var i=0;1<menu_list_array.length;i++){
         
-        htmldata=htmldata+'<div class-"card">' +'imgsrc="images/pizzaImg.png"/>' + menu_list_array[i] + '</div>'
+        htmldata=htmldata+'<div class-"card">' +'img src="images/pizzaImg.png"/>' + menu_list_array[i] + '</div>'
     }
-    htmldata=htmldata+"</section>"
+    htmldata=htmldata+"</div>"
     document.getElementById("display_addmenu").innerHTML = htmldata;
 }
